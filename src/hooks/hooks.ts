89fd88a235a3d176fa6f38c75aaf7f5a95d10bbc
Sergey <Zaikouski.sergey@gmail.com>
2023-07-05 14:04:13 +0300

    15 lesson start

--- src/hooks/hooks.ts
+++
@@ -1,7 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux'
-import {AppDispatch, AppRootStateType} from "../state/store";
-
-// Use throughout your app instead of plain useDispatch and useSelector
-export const useAppDispatch = () => useDispatch<AppDispatch>() //для санок
-// export const useAppDispatch = () => useDispatch<typeof store.dispatch>();
-export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
