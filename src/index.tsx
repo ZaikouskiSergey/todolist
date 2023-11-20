5310d9cb855d9baa87f2719abc94bf0ea07d0959
Sergey <Zaikouski.sergey@gmail.com>
2023-11-20 13:17:56 +0200

    fix: package.json

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,16 @@ import reportWebVitals from './reportWebVitals';
 import App from './app/App';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <HashRouter >
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter >
 );
 reportWebVitals();
